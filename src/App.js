db3a82a46ed0618fef352ce63fa521c0b945e260
prince <itsprince25k@gmail.com>
2023-04-28 11:01:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  let time = new Date().toLocaleTimeString();
+
+  const [ctime , setCtime]= useState(time);
+
+  const  updatetime =()=>{
+    let time = new Date().toLocaleTimeString();
+    setCtime(time);
+  }
+  setInterval (updatetime,1000)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h2>Digital Clock</h2>
+    <h1>{ctime}</h1>
+    {/* <button onClick={updatetime}>get time </button> */}
+    </>
   );
 }
 
